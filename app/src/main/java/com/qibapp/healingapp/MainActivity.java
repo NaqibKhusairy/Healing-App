1266750636d21470858a7c8fbe32c6c916e1e218
NaqibKhusairy <naaqibkhusairy@gmail.com>
2023-08-16 01:44:22 +0800

    Initial commit

---
+++ app/src/main/java/com/qibapp/healingapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.qibapp.healingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
